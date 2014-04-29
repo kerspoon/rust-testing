b1cd881b26f399651832da8438e7cde7bd64fa35
James Brooks <kerspoon@gmail.com>
2014-04-29 23:34:13 +0100

    basic eval working

--- eval-lisp.rs
+++ eval-lisp.rs
@@ -2,6 +2,8 @@
 extern crate collections;
 use collections::HashMap;
 use std::gc::Gc;
+use std::from_str::FromStr;
+use std::fmt;
 
 enum Value {
   Nil,
@@ -10,6 +12,27 @@ enum Value {
   Number(int),
   Pair(~Value, ~Value),
   Lambda(~Value, ~Value)
+}
+
+// http://static.rust-lang.org/doc/0.10/std/from_str/trait.FromStr.html
+impl FromStr for Value {
+  fn from_str(s: &str) -> Option<Value> {
+    if s[0] == '4' as u8 {
+      Some(Number(4))
+    } else {
+      None
+    }
+  }
+}
+
+// https://github.com/mozilla/rust/blob/master/src/test/run-pass/ifmt.rs
+impl fmt::Show for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      match self {
+        &Symbol(ref x) => f.buf.write(x.as_bytes()),
+        _ => f.buf.write("other".as_bytes())
+      }
+    }
 }
 
 fn gen_symbol(map : &mut HashMap<~str,Gc<Value>>, name: ~str) -> Gc<Value>{
@@ -23,7 +46,14 @@ fn gen_symbol(map : &mut HashMap<~str,Gc<Value>>, name: ~str) -> Gc<Value>{
 }
 
 fn evalLisp(this: Gc<Value>, env: &mut HashMap<~str,Gc<Value>>) -> Gc<Value> {
-  Gc::new(Symbol(~"nil"))
+
+  match this.borrow() {
+    &Symbol(ref s) => {
+      // TODO: look up in environment.
+      this.clone()
+    },
+    _ => gen_symbol(env, ~"nil")
+  }
 }
 
 fn main() {
@@ -33,7 +63,9 @@ fn main() {
 
   gen_symbol(&mut env, ~"nil");
 
-  evalLisp(program, &mut env);
+  let res = evalLisp(program, &mut env);
+
+  println!("res {}", res.borrow());
 }
 
 // put food in fridge
