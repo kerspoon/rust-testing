8da83657c76198a8e96d4af37c5b061b33cba6ac
James Brooks <kerspoon@gmail.com>
2014-05-09 22:52:19 +0100

    created basic env

--- environment.rs
+++ environment.rs
@@ -13,23 +13,73 @@ enum Value {
 }
 
 struct Env {
-  elms : HashMap<~str,Gc<Value>>,
-  parent : Option<~Env>
+  frame : HashMap<~str,Gc<Value>>,
+  parent : Option<Gc<Env>>
 }
 
-fn envGet(env: &mut Env, name: ~str) -> Gc<Value> {
-
+// get from the environment a value.
+// if it is not in this frame then look in parent
+// if there is no parent then return Nil.
+// should this be Nil or error in some other way.
+fn envGet(env: & Env, key: ~str) -> Gc<Value> {
+  if env.frame.contains_key(&key) {
+    env.frame.get(&key).clone()
+  } else {
+    match env.parent {
+      None => Gc::new(Nil),
+      Some(ref parent) => {
+        let tmp = parent.borrow();
+        envGet(tmp, key)
+      }
+    }
+  }
 }
 
 fn envAdd(env: &mut Env, key: ~str, val: Gc<Value>) {
-
+  env.frame.insert(key.clone(), val.clone());
 }
 
-// I'll probably need garbage collection here for the returned value
-fn envNewFrame(env: &mut Env) -> Env {
+fn test_basic() {
+  let mut env = Env {
+    frame: HashMap::<~str,Gc<Value>>::new(),
+    parent: None
+  };
 
+  let num = Gc::new(Number(4));
+  envAdd(&mut env, ~"hi", num.clone());
+  let num2 = envGet(&mut env, ~"hi");
+  assert!(num.ptr_eq(&num2));
+}
+
+
+// I'll probably need garbage collection here for the returned value
+fn envNewFrame(env: &mut Gc<Env>) -> Gc<Env> {
+  Gc::new(Env {
+    frame: HashMap::<~str,Gc<Value>>::new(),
+    parent: Some(env.clone())
+  })
+}
+
+fn test_new_frame() {
+  let mut env = Gc::new(Env {
+    frame: HashMap::<~str,Gc<Value>>::new(),
+    parent: None
+  });
+
+  let newEnv = envNewFrame(&mut env);
+  let newEnvBorrow = newEnv.borrow();
+
+  match newEnvBorrow.parent {
+    Some(thing) => {
+      assert!(env.ptr_eq(& thing));
+    }
+    _ => {
+      fail!(~"expected parent to exist");
+    }
+  }
 }
 
 fn main() {
-
+  test_basic();
+  test_new_frame();
 }
